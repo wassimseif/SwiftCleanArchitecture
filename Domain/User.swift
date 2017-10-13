8dbc04b26596d0024bde17614ff0e1626c11294c
Wassim Seifeddine <wassim_seifeddine95@live.com>
2017-10-13 07:12:47 +0300

    Removed Biolerplate code

--- Domain/User.swift
+++ Domain/User.swift
@@ -6,38 +6,8 @@
 //  Copyright © 2017 wassimseif. All rights reserved.
 //
 
-import Foundation
+import Foundation\
 public class User : Codable{
     
 }
 
-
-class UserNetworkRepository : Repository{
-    func getAll() -> [User] {
-        return []
-    }
-    
-    func getById(id: String) -> User {
-        return User()
-    }
-    
-    func insert(item: User) {
-        
-    }
-    
-    func update(item: User) {
-        
-    }
-    
-    func clean() {
-        
-    }
-    
-    func deleteById(id: String) {
-        
-    }
-    
-    typealias T = User
-    
-                                    
-}
